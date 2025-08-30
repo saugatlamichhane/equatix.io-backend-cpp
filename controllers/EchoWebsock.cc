5745894db4bb1df0fba9f161e8ecec8ab266760f
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2025-08-30 09:26:47 -0500

    first commit

---
+++ controllers/EchoWebsock.cc
@@ -0,0 +1,45 @@
+//EchoWebsock.cc
+#include "EchoWebsock.h"
+#include <drogon/HttpTypes.h>
+#include <drogon/PubSubService.h>
+#include <drogon/WebSocketConnection.h>
+#include <memory>
+#include <trantor/utils/Logger.h>
+
+struct Subscriber {
+    std::string chatRoomName_;
+    drogon::SubscriberID id_;
+};
+
+void EchoWebsock::handleNewMessage(const WebSocketConnectionPtr &wsConnPtr,std::string &&message, const WebSocketMessageType& type)
+{
+    //write your application logic here
+    LOG_DEBUG << "new websocket message: " << message;
+    if(type == WebSocketMessageType::Ping) {
+        LOG_DEBUG << "recv a ping";
+    } else if(type == WebSocketMessageType::Text) {
+        auto &s = wsConnPtr->getContextRef<Subscriber>();
+        chatRooms_.publish(s.chatRoomName_, message);
+    }
+}
+void EchoWebsock::handleNewConnection(const HttpRequestPtr &req,const WebSocketConnectionPtr &wsConnPtr)
+{
+    //write your application logic here
+    LOG_DEBUG << "new websocket connection!";
+
+    wsConnPtr->send("haha!!");
+    Subscriber s;
+    s.chatRoomName_ = req->getParameter("room_name");
+    s.id_ = chatRooms_.subscribe(s.chatRoomName_, [wsConnPtr] (const std::string &topic, const std::string &message) {
+            (void) topic;
+            wsConnPtr->send(message);
+            });
+    wsConnPtr->setContext(std::make_shared<Subscriber>(std::move(s)));
+}
+void EchoWebsock::handleConnectionClosed(const WebSocketConnectionPtr &wsConnPtr)
+{
+    //write your application logic here
+    LOG_DEBUG << "websocket closed!";
+    auto &s = wsConnPtr->getContextRef<Subscriber>();
+    chatRooms_.unsubscribe(s.chatRoomName_, s.id_);
+}
---
+++ controllers/EchoWebsock.h
@@ -0,0 +1,22 @@
+//EchoWebsock.h
+#pragma once
+#include <drogon/WebSocketController.h>
+#include <drogon/PubSubService.h>
+#include <drogon/HttpAppFramework.h>
+using namespace drogon;
+class EchoWebsock:public drogon::WebSocketController<EchoWebsock>
+{
+public:
+    virtual void handleNewMessage(const WebSocketConnectionPtr&,
+                                std::string &&,
+                                const WebSocketMessageType &)override;
+    virtual void handleNewConnection(const HttpRequestPtr &,
+                                    const WebSocketConnectionPtr&)override;
+    virtual void handleConnectionClosed(const WebSocketConnectionPtr&)override;
+    WS_PATH_LIST_BEGIN
+    //list path definitions here;
+    WS_PATH_ADD("/echo");
+    WS_PATH_LIST_END
+private:
+        PubSubService<std::string> chatRooms_;
+};
