bd6c99b4d34214ffc254cef4026cf68e48256937
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2025-09-07 12:39:45 -0500

    profile

---
+++ controllers/Profile.cc
@@ -0,0 +1,45 @@
+#include "Profile.h"
+#include <drogon/HttpAppFramework.h>
+#include <drogon/HttpRequest.h>
+#include <drogon/HttpResponse.h>
+#include <drogon/orm/Exception.h>
+#include <drogon/orm/Mapper.h>
+#include <models/Users.h>
+#include <trantor/utils/Logger.h>
+
+using namespace drogon::orm;
+using namespace drogon_model::equatix;
+
+Profile::Profile() { LOG_DEBUG << "Profile controller initialized"; }
+
+void Profile::getInfo(
+    const drogon::HttpRequestPtr &req,
+    std::function<void(const drogon::HttpResponsePtr &)> &&callback,
+    int uid) const {
+  auto client = drogon::app().getDbClient();
+  Mapper<Users> mp(client);
+
+  mp.findByPrimaryKey(
+      uid,
+      [callback](Users profile) {
+        Json::Value result;
+        result["id"] = profile.getValueOfId();
+                result["name"] = profile.getValueOfName();
+                result["uid"] = profile.getValueOfUid();
+                result["email"] = profile.getValueOfEmail();
+                result["photo"] = profile.getValueOfPhoto();
+                result["elo"] = profile.getValueOfElo();
+                result["games played"] = profile.getValueOfGamesplayed();
+                result["wins"] = profile.getValueOfWins();
+                result["losses"] = profile.getValueOfLosses();
+                result["draws"] = profile.getValueOfDraws();
+                callback(drogon::HttpResponse::newHttpJsonResponse(result));
+      },
+      [callback](const DrogonDbException &e) {
+        Json::Value error;
+        error["error"] = e.base().what();
+        callback(drogon::HttpResponse::newHttpJsonResponse(error));
+      }
+
+  );
+}
---
+++ controllers/Profile.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <drogon/HttpController.h>
+#include <drogon/HttpRequest.h>
+#include <drogon/HttpResponse.h>
+#include <drogon/drogon.h>
+
+class Profile : public drogon::HttpController<Profile> {
+    public:
+        METHOD_LIST_BEGIN
+        METHOD_ADD(Profile::getInfo, "/{uid}", drogon::Get);
+        METHOD_LIST_END
+
+        void getInfo(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, int uid) const;
+
+        Profile();
+
+};
