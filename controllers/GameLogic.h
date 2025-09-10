6f40abb1fdde07b7ada4fbf7259065d90cafb4c2
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2025-09-10 16:51:31 -0500

    Revert "modularize"
    This reverts commit 7c3ae663eaf21f58a869c759db32a2cc530a5b87.

--- controllers/GameLogic.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "EchoWebsock.h"
-#include "ValidationHelpers.h"
-#include "BoardHelpers.h"
-#include <drogon/WebSocketController.h>
-#include <json/json.h>
-
-void reset(RoomState &room, int playerTurn);
-bool checkEndGame(RoomState &room);
