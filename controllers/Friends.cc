f95509c67dd25c5175675bc579908da00d7e0b85
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2025-11-08 21:53:24 -0600

    view sent friend requests and cancel

--- controllers/Friends.cc
+++ controllers/Friends.cc
@@ -117,6 +117,51 @@ void Friends::sendFriendRequest(const drogon::HttpRequestPtr& req, std::function
     );
 }
 
+void Friends::getSentFriendRequests(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+    auto client = drogon::app().getDbClient();
+    auto uid = req->attributes()->get<std::string>("uid");
+
+    client->execSqlAsync(
+        "SELECT u.uid, u.name, u.elo, u.photo FROM friends f JOIN users u ON f.friend_uid = u.uid WHERE f.uid=$1 AND f.status='pending'",
+        [callback](const Result &r){
+            Json::Value root; 
+            Json::Value arr(Json::arrayValue);
+            for(const auto &row: r){
+                Json::Value u;
+                u["uid"] = row["uid"].as<std::string>();
+                u["name"] = row["name"].as<std::string>();
+                u["elo"] = row["elo"].as<int>();
+                u["photo"] = row["photo"].as<std::string>();
+                arr.append(u);
+            }
+            root["sentRequests"] = arr;
+            callback(drogon::HttpResponse::newHttpJsonResponse(root));
+        },
+        [callback](const DrogonDbException &e){
+            Json::Value err; err["error"] = e.base().what();
+            callback(drogon::HttpResponse::newHttpJsonResponse(err));
+        }, uid
+    );
+}
+
+void Friends::cancelSentFriendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid){
+    auto client = drogon::app().getDbClient();
+    auto uid = req->attributes()->get<std::string>("uid");
+
+    client->execSqlAsync(
+        "DELETE FROM friends WHERE uid=$1 AND friend_uid=$2 AND status='pending'",
+        [callback](const Result &){
+            Json::Value res; res["success"] = true; res["message"] = "Friend request canceled";
+            callback(drogon::HttpResponse::newHttpJsonResponse(res));
+        },
+        [callback](const DrogonDbException &e){
+            Json::Value res; res["success"] = false; res["error"] = e.base().what();
+            callback(drogon::HttpResponse::newHttpJsonResponse(res));
+        }, uid, friend_uid
+    );
+}
+
+
 void Friends::acceptFriendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid){
     auto client = drogon::app().getDbClient();
     auto uid = req->attributes()->get<std::string>("uid");
--- controllers/Friends.h
+++ controllers/Friends.h
@@ -16,6 +16,11 @@ public:
         // Send friend request
         ADD_METHOD_TO(Friends::sendFriendRequest, "/friend-request/{friend_uid}", drogon::Post, "FirebaseAuthFilter");
 
+        // List sent friend requests
+        ADD_METHOD_TO(Friends::getSentFriendRequests, "/friend-requests/sent", drogon::Get, "FirebaseAuthFilter");
+
+        //Cancel sent friend request
+        ADD_METHOD_TO(Friends::cancelSentFriendRequest, "/friend-request/cancel/{friend_uid}", drogon::Delete, "FirebaseAuthFilter");
         // Accept friend request
         ADD_METHOD_TO(Friends::acceptFriendRequest, "/friend-request/accept/{friend_uid}", drogon::Post, "FirebaseAuthFilter");
 
@@ -31,6 +36,8 @@ public:
     void getFriends(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
     void getFriendRequests(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
     void sendFriendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid);
+    void getSentFriendRequests(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback);
+    void cancelSentFriendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid);
     void acceptFriendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid);
     void rejectFriendRequest(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid);
     void deleteFriend(const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, std::string friend_uid);
